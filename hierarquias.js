0c76eebb928ae3a94eb3b21a94741a181ad6ae45
Mociap <mozer.cielle@gmail.com>
2024-08-29 15:58:15 -0300

    relmensal

---
+++ hierarquias.js
@@ -0,0 +1,70 @@
+//Essas hierarquias estão vinculadas aos cards da página de login
+//Relatórios
+
+const hierarquias = {
+    ADMIN: {
+        nivel: 5,
+        permissoes: [
+            'resumoMensal', 
+            'organograma', 
+            'indicadores', 
+            'relatorioMembros', 
+            'dadosReunioes'
+        ]
+    },
+    Pastor: {
+        nivel: 4,
+        permissoes: [
+            'resumoMensal', 
+            'organograma', 
+            'indicadores', 
+            'relatorioMembros', 
+            'dadosReunioes'
+        ]
+    },
+    Coordenador: {
+        nivel: 3,
+        permissoes: [
+            'resumoMensal', 
+            'organograma', 
+            'indicadores', 
+            'relatorioMembros', 
+            'dadosReunioes'
+        ]
+    },
+    Supervisor: {
+        nivel: 2,
+        permissoes: [
+            'resumoMensal', 
+            'organograma', 
+            'indicadores', 
+            'relatorioMembros', 
+            'dadosReunioes'
+        ]
+    },
+    Lider: {
+        nivel: 1,
+        permissoes: [
+            'organograma', 
+            'indicadores', 
+            'relatorioMembros', 
+            'dadosReunioes'
+        ]
+    },
+    Membro: {
+        nivel: 0,
+        permissoes: [
+            'organograma', 
+            'indicadores', 
+            'relatorioMembros', 
+            'dadosReunioes']
+    }
+};
+
+function temPermissao(funcao, permissao) {
+    return hierarquias[funcao] && hierarquias[funcao].permissoes.includes(permissao);
+}
+
+function getNivelHierarquico(funcao) {
+    return hierarquias[funcao] ? hierarquias[funcao].nivel : -1;
+}
