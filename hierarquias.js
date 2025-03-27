5a19467b33126ae6f23b4d0236cf412df4ea1e28
MOCIAP <mozer.cielle@gmail.com>
2025-03-27 20:37:07 -0300

    hierarquia

--- hierarquias.js
+++ hierarquias.js
@@ -1,66 +1,138 @@
 //Essas hierarquias estão vinculadas aos cards da página de login
 //Relatórios
+//frmEM
 
 const hierarquias = {
     ADMIN: {
         nivel: 5,
         permissoes: [
-            'resumoMensal', 
-            'organograma', 
-            'indicadores', 
-            'relatorioMembros', 
+            'resumoMensal',
+            'organograma',
+            'indicadores',
+            'relatorioMembros',
             'dadosReunioes',
             'relApresentacao',
-            'acessoRapido'
+            'acessoRapido',
+
+            // Escola Ministerial frmEM
+            'inscricoes',
+            'chamada',
+            'frequencia',
+            'admIncricoes',
+            'admTurmas'
         ]
     },
     Pastor: {
         nivel: 4,
         permissoes: [
-            'resumoMensal', 
-            'organograma', 
-            'indicadores', 
-            'relatorioMembros', 
+            'resumoMensal',
+            'organograma',
+            'indicadores',
+            'relatorioMembros',
             'dadosReunioes',
-            'relApresentacao'
+            'relApresentacao',
+
+            // Escola Ministerial frmEM
+            'inscricoes',
+            'chamada',
+            'frequencia',
+            'admIncricoes',
+            'admTurmas'
         ]
     },
     Coordenador: {
         nivel: 3,
         permissoes: [
-            'resumoMensal', 
-            'organograma', 
-            'indicadores', 
-            'relatorioMembros', 
-            'dadosReunioes'
+            'resumoMensal',
+            'organograma',
+            'indicadores',
+            'relatorioMembros',
+            'dadosReunioes',
+
+            // Escola Ministerial frmEM
+            'inscricoes',
+            'chamada',
+            'frequencia',
+            'admIncricoes',
+            'admTurmas'
         ]
     },
     Supervisor: {
         nivel: 2,
         permissoes: [
-            'resumoMensal', 
-            'organograma', 
-            'indicadores', 
-            'relatorioMembros', 
-            'dadosReunioes'
+            'resumoMensal',
+            'organograma',
+            'indicadores',
+            'relatorioMembros',
+            'dadosReunioes',
+
+            // Escola Ministerial frmEM
+            'inscricoes',
+            'chamada',
+            'frequencia',
         ]
     },
     Lider: {
         nivel: 1,
         permissoes: [
-            'organograma', 
-            'indicadores', 
-            'relatorioMembros', 
-            'dadosReunioes'
+            'organograma',
+            'indicadores',
+            'relatorioMembros',
+            'dadosReunioes',
+
+            // Escola Ministerial frmEM
+            'inscricoes'
         ]
     },
     Membro: {
         nivel: 0,
         permissoes: [
-            'organograma', 
-            'indicadores', 
-            'relatorioMembros', 
-            'dadosReunioes']
+            'organograma',
+            'indicadores',
+            'relatorioMembros',
+            'dadosReunioes',
+
+            // Escola Ministerial frmEM
+            'inscricoes'
+        ]
+    },
+
+    // Adicionando funções específicas da Escola Ministerial
+    COORDENADOR: {
+        nivel: 3,
+        permissoes: [
+            // Escola Ministerial frmEM
+            'inscricoes',
+            'chamada',
+            'frequencia',
+            'admIncricoes',
+            'admTurmas'
+        ]
+    },
+    SUPERVISOR: {
+        nivel: 2,
+        permissoes: [
+            // Escola Ministerial frmEM
+            'inscricoes',
+            'chamada',
+            'frequencia'
+        ]
+    },
+    PROFESSOR: {
+        nivel: 1,
+        permissoes: [
+            // Escola Ministerial frmEM
+            'inscricoes',
+            'chamada',
+            'frequencia'
+        ]
+    },
+    Aluno: {
+        nivel: 0,
+        permissoes: [
+            // Escola Ministerial frmEM
+            'inscricoes'
+        ]
     }
 };
 
